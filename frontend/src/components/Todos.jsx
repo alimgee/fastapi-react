61d287379f5b07cdd9285101abeabc75a6b2283a
alanmcgee <mcgee.al@gmail.com>
2022-08-06 12:06:41 +0100

    Adding delete route to api and calling in frontend Todo component

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -124,10 +124,28 @@ export default function Todos() {
             {item}
             <Flex align="end">
               <UpdateTodo item={item} id={id} fetchTodos={fetchTodos}/>
+              <DeleteTodo id={id} fetchTodos={fetchTodos}/> 
             </Flex>
           </Text>
         </Flex>
       </Box>
+    )
+  }
+
+  function DeleteTodo({id}) {
+    const {fetchTodos} = React.useContext(TodosContext)
+  
+    const deleteTodo = async () => {
+      await fetch(`http://localhost:8000/todo/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: { "id": id }
+      })
+      await fetchTodos()
+    }
+  
+    return (
+      <Button h="1.5rem" size="sm" onClick={deleteTodo}>Delete Todo</Button>
     )
   }
 
